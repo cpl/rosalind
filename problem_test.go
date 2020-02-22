69bcc6d2a59ab843efbdcef3c177d398300c8530
Alexandru-Paul Copil <alexandru@cpl.li>
2020-02-22 11:42:08 +0000

    Add solution for problem dna

---
+++ problem_test.go
@@ -0,0 +1,19 @@
+package rosalind
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProblemDNA(t *testing.T) {
+	t.Parallel()
+
+	dna := "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+	count := CountACGT(dna)
+
+	assert.Equal(t, 20, count['A'])
+	assert.Equal(t, 12, count['C'])
+	assert.Equal(t, 17, count['G'])
+	assert.Equal(t, 21, count['T'])
+}
